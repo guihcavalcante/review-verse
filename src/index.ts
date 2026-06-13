4820add1204eeff32f6660ef354f2b58641db6cd
Mateus <mateus12jesusteteu@gmail.com>
2026-06-13 14:46:22 -0300

    Alteração das cores das estrelas.
    Nesta alteração, o componente de avaliação por estrelas foi totalmente refatorado para aprimorar tanto a sua identidade visual quanto a experiência do usuário. No aspecto estético, a paleta de cores foi atualizada para se alinhar ao tema dark da aplicação, integrando efeitos modernos de transição suave, leve ampliação de tamanho e um brilho sutil ao interagir com os elementos.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 let stars =document.querySelectorAll<HTMLElement>('.star')
+let rating = -1
 
 stars.forEach((star, index) => {
     star.addEventListener('click', () => {
-        stars.forEach((s) => {
+
+        if(rating === index){
+            rating = -1
+        } else{
+            rating = index
+        }
+
+        stars.forEach((s, i) => {
+            if(i <= rating){
+                s.classList.add('active')
+            } else {
+                s.classList.remove('active')
+            }
+
             s.style.color = 'gray'
         })
         for(let i = 0; i <= index; i++){
-            stars[i].style.color = 'gold'
+            stars[i].style.color = '#a855f7'
         }
     })
 })
